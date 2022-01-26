893148811f3516346d624f9d032469878526a10d
kje00001 <kje00001@mix.wvu.edu>
2022-01-25 23:25:30 -0500

    Implements the top 2 navbars

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { TopNavBarComponent } from './navigation/TopNavBar/topnavbar.component';
 
 import { AppComponent } from './app.component';
+import { SecNavBarComponent } from './navigation/SecondNavBar/secnavbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavBarComponent,
+    SecNavBarComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/navigation/SecondNavBar/secnavbar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+@Component({
+    selector: 'n-secnavbar',
+    templateUrl: 'secnavbar.component.html',
+    styleUrls: [
+        "secnavbar.component.css"
+]
+})
+
+
+export class SecNavBarComponent{
+    
+}
