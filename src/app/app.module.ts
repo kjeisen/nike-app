61f79003c1e4ce9088f570b1ce44da890888c84f
kje00001 <kje00001@mix.wvu.edu>
2022-01-26 23:27:08 -0500

    Finished up navbars

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { TopNavBarComponent } from './navigation/TopNavBar/topnavbar.component';
+import { TopNavBarComponent } from './navigation/Header/TopNavBar/topnavbar.component';
 
 import { AppComponent } from './app.component';
-import { SecNavBarComponent } from './navigation/SecondNavBar/secnavbar.component';
+import { SecNavBarComponent } from './navigation/Header/SecondNavBar/secnavbar.component';
 
 @NgModule({
   declarations: [
